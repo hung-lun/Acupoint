7059ef100bc9c84ecde91f0deed23d3510aa8dea
hung-lun <s97014280@gmail.com>
2023-10-15 00:57:06 +0800

    refactor: :zap: 更改api取得方式  10150057

--- acu-back/js/back-diagnosis.js
+++ acu-back/js/back-diagnosis.js
@@ -25,7 +25,16 @@ logout1.addEventListener("click", logout);
             console.log(response.data);
             // 將取得資料帶入空陣列data中
             data = response.data;
+            const eightdata = data.slice(0,8);
+            const fourdata = data.slice(8,12);
+            const threedata = data.slice(12,15);
+            console.log(eightdata);
+            data = eightdata;
+            data1 = fourdata;
+            data2 = threedata;
             title(data);
+            title1(data1);
+            title2(data2);
           });
         })();
 
@@ -37,12 +46,14 @@ logout1.addEventListener("click", logout);
           arr.forEach(function (data) {
             str += `
                     <tr>
-                        <td class="Q-title">○　<input type="text" value="${data.eye_question_content}"><input id="${data.eye_question_id}" class="delete mouse" type="button" value="刪除"></td>
+                        <td class="Q-title">○　<input type="text" value="${data.eye_question_content}"><input id="${data.eye_question_id}" class="save mouse" type="button" value="儲存變更"><input id="${data.eye_question_id}" class="delete mouse" type="button" value="刪除"></td>
                     </tr>
             `;
           });
           p_title.innerHTML = str;
+        }
 
+        function title1(arr) {
           // 抓取欄位
           const p_title1 = document.querySelector(".table_content1");
           let str1 = "";
@@ -50,13 +61,15 @@ logout1.addEventListener("click", logout);
           arr.forEach(function (data) {
             str1 += `
                     <tr>
-                        <td class="Q-title">○　<input type="text" value="${data.eye_question_content}"><input id="${data.eye_question_id}" class="delete mouse" type="button" value="刪除"></td>
+                        <td class="Q-title">○　<input type="text" value="${data.eye_question_content}"><input id="${data.eye_question_id}" class="save mouse" type="button" value="儲存變更"><input id="${data.eye_question_id}" class="delete mouse" type="button" value="刪除"></td>
                     </tr>
             `;
           });
           p_title1.innerHTML = str1;
+          }
 
 
+          function title2(arr) {
           // 抓取欄位
           const p_title2 = document.querySelector(".table_content2");
           let str2 = "";
@@ -64,7 +77,7 @@ logout1.addEventListener("click", logout);
           arr.forEach(function (data) {
             str2 += `
                     <tr>
-                        <td class="Q-title">○　<input type="text" value="${data.eye_question_content}"><input id="${data.eye_question_id}" class="delete mouse" type="button" value="刪除"></td>
+                        <td class="Q-title">○　<input type="text" value="${data.eye_question_content}"><input id="${data.eye_question_id}" class="save mouse" type="button" value="儲存變更"><input id="${data.eye_question_id}" class="delete mouse" type="button" value="刪除"></td>
                     </tr>
             `;
           });
